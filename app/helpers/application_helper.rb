06a45f92230893cb56a0e050657c2f983ab789dc
xdite <xdite@handlino.com>
2009-05-09 01:08:04 +0800

    seperate web-theme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def yield_or_default(message, default_message = "")
+     message.nil? ? default_message : message
+   end
 end
